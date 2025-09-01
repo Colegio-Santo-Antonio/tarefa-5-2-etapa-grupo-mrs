58845c12eb5becacc7cac292c61aa5a4c9598eff
sapegal <csa16-6308@colegiosantoantonio.com.br>
2025-09-01 14:19:54 -0300

    Update student.py

--- student.py
+++ student.py
@@ -1,24 +1,17 @@
-
 cartao = input().strip()
-
 digitos = [int(d) for d in cartao]
-
 soma_impares = 0
 soma_pares = 0
-
     posicao = len(digitos) - i  
     valor = digitos[i]
-    
-    if posicao % 2 == 1: 
+        if posicao % 2 == 1: 
         soma_impares += valor
     else:
         dobro = valor * 2
         if dobro >= 10:
             dobro = (dobro // 10) + (dobro % 10)  
         soma_pares += dobro
-
 soma_total = soma_impares + soma_pares
-
 if soma_total % 10 == 0:
     print("Cartão válido")
 else:
