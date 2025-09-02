ec481326a543aac379e3dbd5b59027ab0376f867
Rafaaaaaaabreu <csa21-0183@colegiosantoantonio.com.br>
2025-09-02 17:45:41 -0300

    Update student.py

--- student.py
+++ student.py
@@ -1,15 +1,8 @@
-
 cartao = input().strip()
 digitos = [int(d) for d in cartao]
 impares = digitos[-1::-2]
 pares = digitos[-2::-2]
-pares_dobrados = []
-for d in pares:
-    dobro = d * 2
-    if dobro < 10:
-        pares_dobrados.append(dobro)
-    else:
-        pares_dobrados.append(dobro - 9)
+pares_dobrados = [(d*2 if d*2 < 10 else d*2 - 9) for d in pares]
 total = sum(impares) + sum(pares_dobrados)
 if total % 10 == 0:
     print("Cartão válido")
