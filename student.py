f810b7dcbacbdbe8672ea82385b4e1a53a9bb89b
Rafaaaaaaabreu <csa21-0183@colegiosantoantonio.com.br>
2025-09-02 17:38:55 -0300

    Update student.py

--- student.py
+++ student.py
@@ -1,10 +1,10 @@
 cartao = input().strip()
 digitos = [int(d) for d in cartao]
 
-
+# soma dos dígitos em posição ímpar (contando da direita)
 soma_impares = sum(digitos[-1::-2])
 
-
+# soma dos dígitos em posição par (contando da direita)
 soma_pares = 0
 for d in digitos[-2::-2]:
     dobro = d * 2
@@ -13,10 +13,10 @@ for d in digitos[-2::-2]:
     else:
         soma_pares += dobro
 
-
+# soma total
 soma_total = soma_impares + soma_pares
 
-
+# validação final
 if soma_total % 10 == 0:
     print("Cartão válido")
 else:
